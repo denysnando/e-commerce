c2d07086b3aa4e43c2fa2b4b99297f89e43247d1
Denys <denysnando@gmail.com>
2015-02-05 16:56:25 -0200

    welcomes view now carries images of your product

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -4,6 +4,7 @@ class WelcomesController < ApplicationController
     @categories = Category.limit(10)
     @products = Product.limit(6)
     @best_sellers = Product.limit(1)
+    @banners = Banner.limit(3)
   end
 
 end
