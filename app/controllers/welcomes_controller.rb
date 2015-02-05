adbb323ed2ffedd24da81973e5b58e362ae80979
Denys <denysnando@gmail.com>
2015-02-04 22:51:41 -0200

    Welcomes/index alter
    changing layout  welcomes/index for actual data of the product

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -2,6 +2,8 @@ class WelcomesController < ApplicationController
 
   def index
     @categories = Category.limit(10)
+    @products = Product.limit(6)
+    @best_sellers = Product.limit(1)
   end
 
 end
