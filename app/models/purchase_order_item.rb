1f07acff38e3011b9c7a22d37e0cf5e638f67d50
Denys <denysnando@gmail.com>
2015-02-16 18:19:49 -0200

    added method that generates total purchase order

--- app/models/purchase_order_item.rb
+++ app/models/purchase_order_item.rb
@@ -2,6 +2,10 @@ class PurchaseOrderItem < ActiveRecord::Base
   belongs_to :purchase_order, :inverse_of => :purchase_order_items
   belongs_to :product
 
+  def calc_sub_total
+    amount * sub_total_price
+  end
+
   rails_admin do
     visible false
   end
