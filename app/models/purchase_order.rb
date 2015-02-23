bdfa00a970498ab23cca16284775d6f3fa5c1a26
Denys <denysnando@gmail.com>
2015-02-23 11:28:52 -0300

    removing undue commit

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -9,25 +9,6 @@ class PurchaseOrder < ActiveRecord::Base
   before_save :status_purchase_order!
   before_save :update_total
 
-  state_machine :state, initial: :aberto do
-
-    event :read do
-      transition unread: :read
-    end
-
-    event :archive do
-      transition any => :archived
-    end
-
-    after_transition to: :read do |message|
-      message.read_at = Time.current
-    end
-
-    after_transition to: :archive do |message|
-      message.archived_at = Time.current
-    end
-  end
-
   private
   def status_purchase_order!
     self.purchase_order_status_id = 1
