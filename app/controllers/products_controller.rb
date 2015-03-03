d310085d0539944cb45a402a7263c70172410aa3
Denys <denysnando@gmail.com>
2015-03-03 09:48:41 -0300

    correction due conversion slim

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,14 +9,5 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @products = Product.limit(3)
     @best_sellers = Product.limit(1)
-    form_info
-    @purchase_order_items.product_id = @product.try(:id)
   end
-
-  private
-
-  def form_info
-    @purchase_order_items = PurchaseOrderItem.new
-  end
-
 end
